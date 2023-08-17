b77dd5d181f8b3209458e372309b744ec3acb5ad
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-17 11:34:43 +0545

    fix: code formatted

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000 // Timeout after 5s
-}).then(() => {
-  console.log('Connected to MongoDB')
-}).catch((err) => {
-  console.error('Error connecting to MongoDB', err)
 })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
